c57738b681a12e0913d7e78aa97581ab36921ece
Tbar-Ghost <tanut.stamp@gmail.com>
2024-05-19 21:42:04 +0700

    Map2 17/05

---
+++ GameTNW/Assets/Script/DialogContainer.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogContainer : MonoBehaviour
+{
+    public List<NpcDialog> dialogs; // ลิสต์ของ Dialog objects
+}
